2edd5da3e2dc35a4c754f4226527ced39c337bd9
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-04-24 19:10:05 -0300

    scripts e sqlite

---
+++ jest-int.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    clearMocks: true,
+    testEnvironment: 'node',
+    preset: 'ts-jest',
+    testMatch: ['<rootDir>/src/**/tests/*.int-spec.ts']
+}
--- src/interfaces/users-repository.interface.ts
+++ src/interfaces/users-repository.interface.ts
@@ -6,3 +6,14 @@ export interface UsersRepositoryInterface {
   findById: (id: string) => Promise<User | undefined>;
   findByUsername: (username: string) => Promise<User | undefined>;
 }
+
+
+/**
+ * Aqui criamos uma interface que irá limitar os modelos de acesso onde ela for usada 
+ * ex: 
+ *    olhar a create-user.usecase.ts onde os metodos create, list, findById e findByUsername são usados 
+ * 
+ * alem disso esta interface tbm é usada na class UsersPrismaRepository implements UsersRepositoryInterface
+ * 
+ * onde estes metodos são implementados.
+ */
